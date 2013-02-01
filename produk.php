ba5e162e70401429bfd11359d442b93d2431dfc2
yunita <unie_221@yahoo.co.id>
2013-02-01 14:16:43 +0700

    produk

---
+++ produk.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="200" border="0" bordercolor="#FF9966">
+  <tr>
+    <td bordercolor="#FF9966"><img src="images/ALIA-1.jpg" width="220" height="285" /></td>
+    <td bordercolor="#FF0000"><img src="images/AZIMA.jpg" width="220" height="285" /></td>
+    <td bordercolor="#FF9966"><img src="images/DURRAH.jpg" width="220" height="285" /></td>
+  </tr>
+  <tr>
+<td bordercolor="#FF9966"><p><strong>ALIA
+    </strong></p>
+    <p>Batik cap semi tulis dengan bahan katun morisen bahan aplikasi velvet (silk)</p>
+    <p>Ukuran Lingkar Dada (LD) ukuran L=90cm, M=88cm, Weight : 500 gram</p>
+    <p>Size : 
+      <select name="select2" id="select2">
+        <option>S (Small)</option>
+        <option>M (Madium)</option>
+        <option>L (Large)</option>
+      </select>
+    </p>
+    <p>Price : Rp.340.000</p></td>
+    <td bordercolor="#FF0000"><p><strong>AZIMA
+    </strong></p>
+      <p>Batik cap semi tulis dengan bahan katun morisen. Bahan aplikasi velvet (silk)</p>
+      <p>Ukuran Lingkar Dada (LD) ukuran L=90cm, M=88cm, Weight : 500 gram</p>
+    <p>Size :
+      <label>
+        <select name="select" id="select">
+          <option>S (Small)</option>
+          <option>M (Madium)</option>
+          <option>L (Large)</option>
+        </select>
+        </label>
+    </form>
+    <p>Price : Rp.330.000</p></td>
+    <td bordercolor="#FF9966"><strong>DURRAH</strong>
+      <p>Weight : 500 gram</p>
+      <p>Size :
+        <label>
+        <select name="select3" id="select3">
+            <option>S (Small)</option>
+            <option>M (Madium)</option>
+            <option>L (Large)</option>
+        </select>
+        </label>
+          </form>
+      </p>
+    <p>Price : Rp.310.000</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p></td>
+  </tr>
+  <tr>
+    <td bordercolor="#FF9966"><img src="images/GHALIA.jpg" width="220" height="285" /></td>
+    <td bordercolor="#FF0000"><img src="images/HIMMA.jpg" width="220" height="285" /></td>
+    <td bordercolor="#FF9966"><img src="images/JINAN.jpg" width="220" height="285" /></td>
+  </tr>
+  <tr>
+    <td bordercolor="#FF9966"><p><strong>GHALIA</strong></p>
+      <p> Batik cap semi tulis dengan bahan katun morisen. Bahan aplikasi velvet (silk)      </p>
+      <p>Ukuran Lingkar Dada (LD) ukuran L=90cm, M=88cm, Weight : 500 gram      </p>
+      <p>Size : 
+        <select name="select4" id="select4">
+          <option>S (Small)</option>
+          <option>M (Madium)</option>
+          <option>L (Large)</option>
+        </select>
+      </p>
+    <p> Price : Rp.316.000</p>
+    <p>&nbsp;</p></td>
+    <td bordercolor="#FF0000"><p><strong>HIMMA</strong></p>
+      <p>Batik cap semi tulis dengan bahan katun morisen. Bahan aplikasi velvet (silk)        </p>
+      <p>Free Belt</p>
+      <p>Ukuran Lingkar Dada (LD) ukuran L=90cm, M=88cm, Weight : 500 gram </p>
+      <p>Size : 
+        <select name="select5" id="select5">
+          <option>S (Small)</option>
+          <option>M (Madium)</option>
+          <option>L (Large)</option>
+        </select>
+      </p>
+    <p>Price : Rp.340.000</p></td>
+    <td bordercolor="#FF9966"><p><strong>JINAN</strong></p>
+      <p>Batik cap semi tulis dengan bahan katun morisen. Bahan aplikasi velvet (silk) </p>
+      <p>With Belt</p>
+      <p>Ukuran Lingkar Dada (LD) ukuran L=90cm, M=88cm, Weight : 500 gram </p>
+      <p>Size :
+        <select name="select6" id="select6">
+            <option>S (Small)</option>
+            <option>M (Madium)</option>
+            <option>L (Large)</option>
+        </select>
+      </p>
+    <p>Price : Rp.340.000</p></td>
+  </tr>
+  <tr>
+    <td bordercolor="#FF9966"><img src="images/NAFISAH.jpg" width="220" height="285" /></td>
+    <td bordercolor="#FF0000"><img src="images/NAJIHA.jpg" width="220" height="285" /></td>
+    <td bordercolor="#FF9966"><img src="images/QARIRA.jpg" width="220" height="285" /></td>
+  </tr>
+  <tr>
+    <td bordercolor="#FF9966"><p><strong>NAFISAH</strong></p>
+      <p>Batik cap semi tulis dengan bahan katun morisen bahan Aplikasi Velvet (Silk)</p>
+      <p>Ukuran Lingkar Dada (LD) ukuran L=90 cm, M=88 cm, S=85</p>
+      <p>Weight=500 gram</p>
+      <p>Size
+        <label>
+          <select name="select" size="1">
+            <option value="Small">S (Small)</option>
+            <option value="Medium">M (Medium)</option>
+            <option value="Large">L (Large)</option>
+          </select>
+          </label>
+      </p>
+    <p>Price Rp. 310.000 </p></td>
+    <td bordercolor="#FF0000"><p><strong>NAJIHA</strong></p>
+      <p>Batik cap kombinasi tulis dengan bahan katun morisen Aplikasi Velvet (Silk)</p>
+      <p>Ukuran Lingkar Dada (LD) ukuran L=90 cm, M=88 cm, S=85</p>
+      <p>Weight=500 gram</p>
+      <p>Size
+        <label>
+          <select name="select2" size="1">
+            <option value="Small">S (Small)</option>
+            <option value="Medium">M (Medum)</option>
+            <option value="Large">L (Large)</option>
+          </select>
+        </label>
+      </p>
+    <p>Price Rp. 236.000 </p></td>
+    <td bordercolor="#FF9966"><p><strong>QARIRA (With Belt)</strong></p>
+      <p>Batik cap semi tulis dengan bahan katun morisen bahan Aplikasi Velvet (Silk)</p>
+      <p>Ukuran Lingkar Dada (LD) ukuran L=90 cm, M=88 cm, S=85</p>
+      <p>Weight=500 gram</p>
+      <p>Size
+        <label>
+          <select name="select3" size="1">
+            <option value="Small">S (Small)</option>
+            <option value="Medium">M (Medium)</option>
+            <option value="Large">L (Large)</option>
+          </select>
+          </label>
+      </p>
+    <p>Price Rp. 340.000 </p></td>
+  </tr>
+  <tr>
+    <td bordercolor="#FF9966"><img src="images/SARAHA.jpg" width="220" height="285" /></td>
+    <td bordercolor="#FF0000"><img src="images/ZAHIYA.jpg" width="220" height="285" /></td>
+    <td bordercolor="#FF9966"><img src="images/MALAHATI.jpg" width="220" height="285" /></td>
+  </tr>
+  <tr>
+    <td bordercolor="#FF9966"><p><strong>SARAHA</strong></p>
+      <p>Batik cap semi tulis dengan bahan katun morisen bahan Aplikasi Velvet (Silk)</p>
+      <p>Ukuran Lingkar Dada (LD) ukuran L=90 cm, M=88 cm, S=85</p>
+      <p>Weight=500 gram</p>
+      <p>Size
+        <label>
+          <select name="select4" size="1">
+            <option value="Small">S (Small)</option>
+            <option value="Medium">M (Medium)</option>
+            <option value="Large">L (Large)</option>
+          </select>
+        </label>
+      </p>
+    <p>Price Rp. 340.000 </p></td>
+    <td bordercolor="#FF0000"><p><strong>ZAHIYA</strong></p>
+      <p>Batik cap semi tulis dengan bahan katun morisen bahan Aplikasi Velvet (Silk)</p>
+      <p>Ukuran Lingkar Dada (LD) ukuran L=90 cm, M=88 cm, S=85</p>
+      <p>Weight=500 gram</p>
+      <p>Size
+        <label>
+          <select name="select5" size="1">
+            <option value="Small">S (Small)</option>
+            <option value="Medium">M (Medium)</option>
+            <option value="Large">L (Large)</option>
+          </select>
+          </label>
+      </p>
+    <p>Price Rp. 316.000 </p></td>
+    <td bordercolor="#FF9966"><p><strong>MALAHATI</strong></p>
+      <p>Batik cap semi tulis dengan bahan katun morisen bahan Aplikasi Velvet (Silk)</p>
+      <p>Ukuran Lingkar Dada (LD) ukuran L=90 cm, M=88 cm, S=85</p>
+      <p>Weight=500 gram</p>
+      <p>Size
+        <label>
+          <select name="select6" size="1">
+            <option value="Small">S (Small)</option>
+            <option value="Medium">M (Medium)</option>
+            <option value="Large">L (Large)</option>
+          </select>
+          </label>
+      </p>
+    <p>Price Rp. 326.000 </p></td>
+  </tr>
+</table>
+</body>
+</html>
