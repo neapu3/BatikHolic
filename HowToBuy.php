1b3703421066cdcac72e8ac97678248e957b569c
yunita <unie_221@yahoo.co.id>
2013-02-01 14:07:42 +0700

    how to buy

---
+++ HowToBuy.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+.kepsek{
+	color:#03C;
+	font-family:"Arial Black", Gadget, sans-serif;
+	font-size:18px;
+	font-style:inherit;
+}
+
+</style>
+</head>
+
+<body>
+<table width="70%" border="0">
+  <tr>
+    <td align="center">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td class="kepsek">Bagaimana cara Berbelanja Di Holic Indonesia</td>
+  </tr>
+  <tr>
+  
+    <td align="justify"><p>1. Cari produk yang anda inginkan di menu produk    
+    <p>2. Klik pada tombol kranjang belanja    
+    <p>3. Klik pada tombol lanjutkan    
+    <p>4. Informasi keranjang belanja akan tampil pada bagian kanan atas web site    
+    <p>5. Jika telah selesai belanja, silahkan klik check out untuk memproses pesanan    
+    <p>6. Cek tipe, harga dan Qty barang yang dipesan    
+    <p>7. Isi informasi data dan pengiriman lalu pilih I Agree to the term and condition    
+    <p>8. Anda akan menerima informasi pesanan via email lalu kami akan mengirimkan informasi rekening pembayaran ke email Anda    
+    <p>9. Harga belom termasuk ongkos kirim    
+    <p>10. Setelah kami menerima </td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+</body>
+</html>
