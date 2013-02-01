152b8bf0489e05681347af103531338856041046
Nea Budiarti <nea_pu3@yahoo.co.id>
2013-02-01 15:17:12 +0700

    lokasi holic

---
+++ lokasi_holic.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="469" border="1" bordercolor="#FF9900">
+  <tr>
+    <td width="463"><h1><font face="Courier New, Courier, monospace"><font color="#0000FF">Lokasi Holic Indonesia</font></h1></td>
+  </tr>
+  <tr>
+    <td><p><b><font face="Georgia, Times New Roman, Times, serif">Medan</font></b></p>
+    <p>Jln. Mulawarman No.19 Medan </p></td>
+  </tr>
+  <tr>
+    <td><p><b><font face="Geneva, Arial, Helvetica, sans-serif">Jakarta</font></b></p>
+    <p>Jln. Taman Kota No.18 jakarta Barat </p></td>
+  </tr>
+  <tr>
+    <td><p><b><font face="Geneva, Arial, Helvetica, sans-serif">Bandung<font></b></p>
+    <p>Jln. Antapati No.21 Bandung </p></td>
+  </tr>
+  <tr>
+    <td><p><b><font face="Geneva, Arial, Helvetica, sans-serif">Yogyakarta<font></b></p>
+    <p>Jln. Malioboro No.23 Jogyakarta </p></td>
+  </tr>
+</table>
+
+</body>
+</html>
