07dd5ee751397797da876b60b1dfc717ee326390
muliarahmayu <muli_mulia@yahoo.com>
2013-02-01 15:04:08 +0700

    Halaman Berita

---
+++ halaman_berita.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="100%" border="1">
+  <tr>
+    <td width="30%" height="45"><h1>Berita Terkini</h1></td>
+    <td width="70%" rowspan="2"><p>Jakarta - Hujan besar disertai angin kencang melanda kawasan Jakarta Barat. Akibatnya banyak pohon yang tumbang. Salah satunya di kawasan dekat lampu merah Kedoya, Jalan panjang, Kedoya, Jakarta Barat. </p>
+    <p>Pantauan, Kamis (10-01-2013) pukul 17:00 WIB pohon jenis angsana panah menimpahmobil Kijang silver dan sebuah motor Yamaha Jupiter MX. Akibatnya pengendara motor yang diperkirakan berumur 22 tahun itu mengalami luka dibagian pinggang. </p></td>
+  </tr>
+  <tr>
+    <td><h3>Pohon Tumbang di Jalan Panjang, 1 orang jadi korban</h3></td>
+  </tr>
+</table>
+<table width="100%" border="1">
+  <tr>
+    <td><h1>Komentar : </h1>
+      <label>
+      <textarea name="textarea" id="textarea" cols="45" rows="5"></textarea>
+      <br />
+      <br />
+      <input type="submit" name="button" id="button" value="Kirim" />
+      </label></td>
+    
+  </tr>
+</table>
+
+</body>
+</html>
