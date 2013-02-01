11fda4e1ed9e1c3456454d20225cc9adb87be192
Nea Budiarti <nea_pu3@yahoo.co.id>
2013-02-01 12:06:09 +0700

    add index
    menambahkan file index ke repository

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<style type="text/css">
+body{ background-image:url(images/backg.gif); margin:0; padding:0; }
+#wrapper{margin:0 auto; width:1010px; margin-top:50px;}
+#topnavi{background:url(images/top.png) repeat-x; top:0; width:100%; height:45px; padding-top:4px; position:fixed; z-index:1000;}
+.lengkung{
+border:#996600 3px solid;
+border-bottom-left-radius:10px;
+border-bottom-right-radius:10px;
+border-top-left-radius:25px;
+border-top-right-radius:25px;
+}
+.lengkung2{
+border:#FFCC33 2px solid;
+border-bottom-left-radius:10px;
+border-bottom-right-radius:10px;
+border-top-left-radius:10px;
+border-top-right-radius:10px;
+}
+.menu {
+	margin: 0;
+	padding: 0;
+	list-style: none;
+	
+}
+.menu li {
+	padding: 0;
+	margin: 0;
+	height: 39px;
+	margin-left:1em;
+	margin-right: 10px;
+	list-style: none;
+	background-repeat: no-repeat;
+}
+.menu li a, .menu li a:visited {
+	display: block;
+	text-decoration: none;
+	text-indent: -9999px;
+	height: 39px;
+	background-repeat: no-repeat;
+}
+.home {background-image: url(images/home.gif); width:108px;}
+.home a {background-image: url(images/home1.gif); width:108px;}
+.product {background-image: url(images/produk.gif); width:108px;}
+.product a {background-image: url(images/produk1.gif); width:108px;}
+.howtobuy {background-image: url(images/how.gif); width:108px;}
+.howtobuy a {background-image: url(images/how1.gif); width: 108px;}
+.contact {background-image: url(images/contact.gif); width:108px;}
+.contact a {background-image: url(images/contact1.gif); width: 108px;}
+.cabang {background-image: url(images/cabang.gif); width: 108px;}
+.cabang a {background-image: url(images/cabang1.gif); width:108px;}
+.visi {background-image: url(images/visi.gif); width: 108px;}
+.visi a {background-image: url(images/visi1.gif); width: 108px;}
+.berita {background-image: url(images/berita.gif); width:108px;}
+.berita a {background-image: url(images/berita1.gif); width: 108px;}
+ul.menu li a:hover {background: none;}
+.menu li {float: left;}
+.menu:after {content: "."; display: block; height: 0; clear: both; visibility: hidden;}
+.lengkung21 {border:#FFCC33 2px solid;
+border-bottom-left-radius:10px;
+border-bottom-right-radius:10px;
+border-top-left-radius:10px;
+border-top-right-radius:10px;
+}
+.lengkung22 {border:#FFCC33 2px solid;
+border-bottom-left-radius:10px;
+border-bottom-right-radius:10px;
+border-top-left-radius:10px;
+border-top-right-radius:10px;
+}
+</style>
+<body>
+<div id="topnavi"></div>
+<div id="wrapper">
+<table width="1000" border="0" cellspacing="0" cellpadding="0" align="center" class="lengkung" style="padding-bottom:5px">
+  <tr>
+    <td valign="top"><table width="1000" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td style="background-image:url(images/bannaerfix_01.png); width:1000px; height:176px">&nbsp;</td>
+  </tr>
+  <tr>
+    <td style="background-image:url(images/bannaerfix_02.png); width:1000px; height:49px; background-repeat:no-repeat" valign="top" align="center"><table width="1000" height="40" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td valign="middle" style="padding-left:20px"><ul class="menu">
+	<li class="home"><a href="?page=home">Home</a></li>
+	<li class="visi"><a href="?page=visi">Visi</a></li>
+	<li class="product"><a href="?page=produk">Produk</a></li>
+	<li class="howtobuy"><a href="?page=howtobuy">how</a></li>
+	<li class="contact"><a href="?page=contact">Contact</a></li>
+	<li class="berita"><a href="?page=berita">Berita</a></li>
+	<li class="cabang"><a href="?page=cabang">Cabang</a></li>
+	</ul></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+<table width="990" height="368" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#F5E2A9" class="lengkung2" style="margin-top:3px">
+  <tr>
+    <td width="226" height="266" style="padding:3px" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0" class="lengkung2" bgcolor="#FFFFCC">
+      <tr>
+        <td height="50" align="center" style="padding:5px"><font color="#FF3300" size="4"><strong>Online Support</strong></font> <br />
+		<!-- Id Ym -->
+		  <br /><font color="#000000"><strong>Customer Service 1:</strong></font>&nbsp;<a href="http://edit.yahoo.com/config/send_webmesg?.target=nea_pu3&.src=pg" target="_blank"> <img src="http://opi.yahoo.com/online?u=nea_pu3&m=g&t=2" /></a>
+		  <br /><font color="#000000"><strong>Customer Service 2:</strong></font>&nbsp;<a href="http://edit.yahoo.com/config/send_webmesg?.target=Unie_221&.src=pg" target="_blank"> <img src="http://opi.yahoo.com/online?u=Unie-221&m=g&t=2" /></a>
+	<br /><font color="#000000"><strong>Marketing 1:</strong></font>&nbsp;<a href="http://edit.yahoo.com/config/send_webmesg?.target=muli_mulia&.src=pg" target="_blank"> <img src="http://opi.yahoo.com/online?u=muli_mulia&m=g&t=2" /></a>
+	<br /><font color="#000000"><strong>Marketing 2:</strong></font>&nbsp;<a href="http://edit.yahoo.com/config/send_webmesg?.target=siti_badaris&.src=pg" target="_blank"> <img src="http://opi.yahoo.com/online?u=siti_badaris&m=g&t=2" /></a>
+	
+		
+</td>
+      </tr>
+    </table><table width="100%" border="0" cellspacing="0" cellpadding="0" class="lengkung2" bgcolor="#FFFFCC" style="margin-top:10px">
+      <tr>
+        <td height="50" align="center" style="padding:5px"><font color="#FF3300" size="4"><strong>Pembayaran</strong></font> <br />
+          <br /><img src="images/bca-icon.jpg" /><br />
+          <font color="#000000"><strong>756 009 8485<br />
+Holic Indonesia</strong></font>
+		 <br/>
+		  <br /><img src="images/bankmandiri.jpg" /><br />
+          <font color="#000000"><strong>8888 999 9999<br />
+Holic Indonesia</strong></font></td>
+      </tr>
+    </table><table width="100%" border="0" cellspacing="0" cellpadding="0" class="lengkung2" bgcolor="#FFFFCC" style="margin-top:10px">
+      <tr>
+        <td height="50" align="center" style="padding:5px"><font color="#FF3300" size="4"><strong>Pengiriman</strong></font> <br />
+		<!-- Banking -->
+		
+		<br /><img src="images/JNE.png" /><br />
+         
+		 <br/>
+		  <br /><img src="images/TIKI-ok.jpg" /><br />
+        <br />  
+	</td>
+      </tr>
+    </table>
+    <table width="100%" border="0" cellspacing="0" cellpadding="0" class="lengkung22" bgcolor="#FFFFCC" style="margin-top:10px">
+      <tr>
+        <td height="50" align="center" style="padding:5px"><!-- Facebook Badge START --><a href="http://www.facebook.com/pelangisenja.telukpanjie" target="_TOP" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;" title="Nea Budiarti">Nea Budiarti</a><br/><a href="http://www.facebook.com/pelangisenja.telukpanjie" target="_TOP" title="Nea Budiarti"><img src="http://badge.facebook.com/badge/1323044758.412.1167284775.png" style="border: 0px;" /></a><br/><a href="http://www.facebook.com/badges/" target="_TOP" style="font-family: &quot;lucida grande&quot;,tahoma,verdana,arial,sans-serif; font-size: 11px; font-variant: normal; font-style: normal; font-weight: normal; color: #3B5998; text-decoration: none;" title="Holic on Facebook">Holic on Facebook</a><!-- Facebook Badge END --></td>
+     	  </tr>
+    </table></td>
+	 
+    <td width="1"></td>
+    <td width="750" style="padding:3px" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0" class="lengkung21" bgcolor="#FFFFCC">
+      <tr>
+        <td></td>
+		 <td><marquee><font size="+4" face="monotype corsiva">Raih Diskon Tahun Baru 50%... Happy Shopping</font></marquee></td>
+      </tr>
+      
+    </table>
+    <table width="100%" border="0" cellspacing="0" cellpadding="0" class="lengkung21" bgcolor="#FFFFCC">
+      <tr>
+        <td></td>
+		 <td><? include"bukafile.php"; ?></td>
+      </tr>
+      
+    </table>
+    </td>
+  </tr>
+</table>
+</td>
+  </tr>
+</table>
+</div>
+</body>
+</html>
